5698f0e911ae381248780a8708656944727f38d4
Dawid <lukasiewicz@onet.com.pl>
2022-10-16 17:18:54 +0200

    random led pattern finished

--- Rpi-pico-W/code/animations.cpp
+++ Rpi-pico-W/code/animations.cpp
@@ -1,2 +1,33 @@
 #include "animations.hpp"
 
+#include <cstdlib>
+#include <ctime>
+
+typedef uint8_t flag;
+
+void random_led(cube &Cube)
+{
+    if (Cube.get_display_state() == 0)
+    {
+        uint8_t x = std::rand()%5;
+        uint8_t y = std::rand()%5;
+        uint8_t z = std::rand()%5;
+
+        Cube.add_led(X_table[x], Y_table[y], Z_table[z]);
+        Cube.display(500);
+    }
+    else if (Cube.get_display_state() == 1)
+    {
+        Cube.display(500);
+    }
+    else if (Cube.get_display_state() == 2)
+    {
+        Cube.clr_leds();
+        Cube.reset_display_state();
+    }
+}
+
+void rain(cube &Cube)
+{
+
+}
--- Rpi-pico-W/code/animations.hpp
+++ Rpi-pico-W/code/animations.hpp
@@ -1,4 +1,9 @@
 #ifndef ANIMATIONS
 #define ANIMATIONS
 
+#include "cube.hpp"
+
+void random_led(cube &Cube);
+void rain(cube &Cube);
+
 #endif
