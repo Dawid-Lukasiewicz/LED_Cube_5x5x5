96e5946886805268e78352dbb6c298cee2be2a25
Dawid <lukasiewicz@onet.com.pl>
2022-10-23 20:45:13 +0200

    small reorder

--- Rpi-pico-W/code/animations.cpp
+++ Rpi-pico-W/code/animations.cpp
@@ -7,6 +7,11 @@ typedef uint8_t flag;
 
 void random_led(cube &Cube)
 {
+    if (Cube.get_display_state() == 2)
+    {
+        Cube.clr_leds();
+        Cube.reset_display_state();
+    }
     if (Cube.get_display_state() == 0)
     {
         uint8_t x = std::rand()%5;
@@ -14,17 +19,8 @@ void random_led(cube &Cube)
         uint8_t z = std::rand()%5;
 
         Cube.add_led(X_table[x], Y_table[y], Z_table[z]);
-        Cube.display(500);
     }
-    else if (Cube.get_display_state() == 1)
-    {
-        Cube.display(500);
-    }
-    else if (Cube.get_display_state() == 2)
-    {
-        Cube.clr_leds();
-        Cube.reset_display_state();
-    }
+    Cube.display(500);
 }
 
 void rain(cube &Cube)
