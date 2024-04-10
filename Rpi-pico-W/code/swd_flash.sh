c8ace96b04ab0d4d59f97986b0432f57578120bd
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2024-04-10 20:57:43 +0200

    run script  with sudo notification

--- Rpi-pico-W/code/swd_flash.sh
+++ Rpi-pico-W/code/swd_flash.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
-sudo openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program build/main.elf verify reset exit"
-# sudo openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program build/main.elf verify reset"
+# Run the script with sudo
+openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program build/main.elf verify reset exit"
+# openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program build/main.elf verify reset"
